e99f95d11f83b4d08e30854e7f049c0daf84f0c6
kamleshp95 <kamleshpadwal@gmail.com>
2020-06-13 23:08:23 +0530

    First Commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,34 @@
+const Sdata =[
+        {
+            sname: "DARK",
+            imgscr: "https://wallpapercave.com/wp/wp4056410.jpg",
+            title: "Netflix Original Series",
+            links: "https://www.netflix.com/in/title/80100172?source=35",
+        },
+        {
+            sname: "Extra Curricular",
+            imgscr: "https://vignette.wikia.nocookie.net/netflix/images/3/3f/Extracurricular_Characters_%284%29.jpg/revision/latest?cb=20200411213211",
+            title: "Netflix Original Series",
+            links: "https://www.netflix.com/in/title/80990668?source=35",
+        },
+        {
+            sname: "Stranger Things",
+            imgscr: "https://wallpapercave.com/wp/wp1917154.jpg",
+            title: "Netflix Original Series",
+            links: "https://www.netflix.com/in/title/80057281",
+        },
+        {
+            sname: "The Vampire Diries",
+            imgscr: "https://images.justwatch.com/poster/8589251/s592",
+            title: "Netflix Original Series",
+            links: "https://www.netflix.com/in/title/70143860",
+        },
+        {
+            sname: "My First First Love",
+            imgscr: "https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg",
+            title: "Netflix Original Series",
+            links: "https://www.netflix.com/in/title/81026700",
+        },
+];
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Card from './Cards';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Sdata from './Sdata';
+
+
+// function ncard(val){
+//   return(
+//     <Card 
+//       imgsrc={val.imgscr}
+//       title={val.title}
+//       sname={val.sname}
+//       link={val.links}
+//     />
+//   );
+// }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <h1 className="heading_style">List of top 5 Netflix Series in 2020</h1>
+    {Sdata.map((val) =>{
+      return(
+        <Card 
+          imgsrc={val.imgscr}
+          title={val.title}
+          sname={val.sname}
+          link={val.links}
+        />
+      );
+    })}
+  </>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
